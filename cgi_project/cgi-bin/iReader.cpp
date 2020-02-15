17359f3ddebfc588bdfafaa8eb2cb4c60eded7b4
rfidspan <44983572+rfidspan@users.noreply.github.com>
2020-02-15 12:52:47 -0800

    Update iReader.cpp

--- cgi_project/cgi-bin/iReader.cpp
+++ cgi_project/cgi-bin/iReader.cpp
@@ -114,6 +114,26 @@ int  IReader::IReaderGetRegion(int *region)
     return (IREADER_SUCCESS);
 }
 
+int  IReader::IReaderGetSearchTimeout(int *timeout)
+{
+	int ret;
+	int reg;
+	unsigned char buf[] = {HDR1, HDR2, 0x00, 0x01, 0xFF, 0xFE, 0x92};  // 0x92 get search timeout
+    // retrieve the list from mux again
+
+	ret = sendmsg(buf);
+
+	if (m_rxMsg.opCode != 0x92 || ret != RFID_CMD_SUCCESS)
+	{
+		return (IREADER_COMMAND_FAIL);
+	}
+
+	reg = (m_rxMsg.data[0] << 8) | (m_rxMsg.data[1]);
+	*timeout = reg;
+
+    return (IREADER_SUCCESS);
+}
+
 
 int  IReader::IReaderSetPowerLevel(int antid, int pwr, int doset)
 {

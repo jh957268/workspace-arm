6c666abd13e7dcc025985cb9f6a6122c9aed7037
rfidspan <44983572+rfidspan@users.noreply.github.com>
2020-03-01 20:02:22 -0800

    Update llrp.cpp

--- llrpreader/llrp-new/llrp.cpp
+++ llrpreader/llrp-new/llrp.cpp
@@ -143,6 +143,7 @@ int main(int argc, char* argv[])
 	/* Create SQL statement */
 
 	char *sql;
+#if 0
 	sql = "CREATE TABLE TAGINFO("  \
 	      "TAGVALUE 	CHAR(24)     NOT NULL," \
 	      "ANTID         INT     NOT NULL," \
@@ -153,6 +154,13 @@ int main(int argc, char* argv[])
 		  "PRIMARY KEY (" \
 		  "TAGVALUE," \
 		  "ANTID));";
+#endif
+	   sql = "CREATE TABLE COMPANY("  \
+	      "ID INT PRIMARY KEY     NOT NULL," \
+	      "NAME           TEXT    NOT NULL," \
+	      "AGE            INT     NOT NULL," \
+	      "ADDRESS        CHAR(50)," \
+	      "SALARY         REAL );";
 
 	   /* Execute SQL statement */
 	   rc = sqlite3_exec(rfid_db, sql, callback, 0, &zErrMsg);
@@ -163,6 +171,28 @@ int main(int argc, char* argv[])
 	   } else {
 	      fprintf(stdout, "Table created successfully\n");
 	   }
+
+	   /* Create SQL statement */
+	    sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
+	          "VALUES (1, 'Paul', 32, 'California', 20000.00 ); " \
+	          "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
+	          "VALUES (2, 'Allen', 25, 'Texas', 15000.00 ); "     \
+	          "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
+	          "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );" \
+	          "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
+	          "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+
+	    /* Execute SQL statement */
+	    rc = sqlite3_exec(rfid_db, sql, callback, 0, &zErrMsg);
+
+	    if( rc != SQLITE_OK ){
+	       fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	       sqlite3_free(zErrMsg);
+	    } else {
+	       fprintf(stdout, "Records created successfully\n");
+	    }
+
+
 	   sqlite3_close(rfid_db);
 //	LLRP_MntServer abc;
 

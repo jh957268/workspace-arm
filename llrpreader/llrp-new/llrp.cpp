67a9469afa641cc3a27c461ad3aa4d4366fefd83
rfidspan <44983572+rfidspan@users.noreply.github.com>
2020-04-15 17:44:14 -0700

    Update llrp.cpp

--- llrpreader/llrp-new/llrp.cpp
+++ llrpreader/llrp-new/llrp.cpp
@@ -41,11 +41,17 @@ Muxserial *Ser1;
 Muxserial *Ser2;
 
 sqlite3 *rfid_db;
+int select_tag_id;
+char sql_buff[128];
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
    int i;
    for(i = 0; i<argc; i++) {
       printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   if (NotUsed)
+   {
+	   *(int *)NotUsed = atoi(argv[0]);
    }
    printf("\n");
    return 0;
@@ -142,7 +148,7 @@ int main(int argc, char* argv[])
 
 	/* Create SQL statement */
 
-	char *sql;
+	char *sql, *sql1;
 #if 0
 	sql = "CREATE TABLE TAGINFO("  \
 	      "TAGVALUE 	CHAR(24)     NOT NULL," \
@@ -166,7 +172,8 @@ CREATE TABLE TAG_DATA
     tag_last_seen  DATETIME, 
     tag_seen_count integer, 
     tag_out_Of_fov integer
-);		  
+);
+
 BEGIN TRANSACTION;
 insert into TAG_DATA (tag_id, tag_val, tag_antID, tag_RSSI, tag_first_seen, tag_last_seen, tag_seen_count, 	tag_out_Of_fov)
 VALUES
@@ -221,6 +228,7 @@ INSERT INTO prod_mast(prod_name, prod_rate, prod_qc)
 VALUES('Gulha', 55, 'Problems');
 	
 #endif
+#if 0
 	   sql = "CREATE TABLE COMPANY("  \
 	      "ID INT PRIMARY KEY     NOT NULL," \
 	      "NAME           TEXT    NOT NULL," \
@@ -251,6 +259,57 @@ VALUES('Gulha', 55, 'Problems');
 	    /* Execute SQL statement */
 	    rc = sqlite3_exec(rfid_db, sql, callback, 0, &zErrMsg);
 
+
+
+	    if( rc != SQLITE_OK ){
+	       fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	       sqlite3_free(zErrMsg);
+	    } else {
+	       fprintf(stdout, "Records created successfully\n");
+	    }
+#endif
+
+	   sql = "CREATE TABLE TAG_DATA" \
+	    "(" \
+	        "tag_id integer PRIMARY KEY," \
+	        "tag_val text(32) NOT NULL," \
+	        "DATE_TIME_STAMP DATETIME default current_timestamp," \
+	        "tag_antID integer," \
+	    	"tag_RSSI	    DOUBLE," \
+	        "tag_first_seen DATETIME," \
+	        "tag_last_seen  DATETIME," \
+	        "tag_seen_count integer," \
+	        "tag_out_Of_fov integer" \
+	   ");";
+
+	   /* Execute SQL statement */
+	   rc = sqlite3_exec(rfid_db, sql, callback, 0, &zErrMsg);
+
+	   if( rc != SQLITE_OK ){
+	      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	      sqlite3_free(zErrMsg);
+	   } else {
+	      fprintf(stdout, "Table created successfully\n");
+	   }
+
+	   sql = "insert into TAG_DATA (tag_id, tag_val, tag_antID, tag_RSSI, tag_first_seen, tag_last_seen, tag_seen_count, tag_out_Of_fov)" \
+			 "VALUES" \
+	         "(1, '1234567890abcdef1234567f', 1, -55.0, datetime('now'), datetime('now'), 1, 1);";
+
+	   sql1 = "insert into TAG_DATA (tag_val, tag_antID, tag_RSSI, tag_first_seen, tag_last_seen, tag_seen_count, tag_out_Of_fov)" \
+	          "VALUES" \
+	          "('1234567890abcdef12345678', 1, -55.0, datetime('now'), datetime('now'), 1, 1)," \
+	   	      "('1234567890abcdef1234567a', 2, -56.0, datetime('now'), datetime('now'), 1, 1)," \
+	   	      "('1234567890abcdef1234567b', 3, -58.0, datetime('now'), datetime('now'), 1, 1);";
+
+
+	   // 'db' is the pointer you got from sqlite3_open*
+	   sqlite3_exec(rfid_db, "BEGIN TRANSACTION;", NULL, NULL, NULL);
+	   // Any (modifying) SQL commands executed here are not committed until at the you call:
+
+	    /* Execute SQL statement */
+	    rc = sqlite3_exec(rfid_db, sql, callback, 0, &zErrMsg);
+
 	    if( rc != SQLITE_OK ){
 	       fprintf(stderr, "SQL error: %s\n", zErrMsg);
 	       sqlite3_free(zErrMsg);
@@ -258,6 +317,37 @@ VALUES('Gulha', 55, 'Problems');
 	       fprintf(stdout, "Records created successfully\n");
 	    }
 
+	    rc = sqlite3_exec(rfid_db, sql1, callback, 0, &zErrMsg);
+
+	    if( rc != SQLITE_OK ){
+	       fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	       sqlite3_free(zErrMsg);
+	    } else {
+	       fprintf(stdout, "Records created successfully\n");
+	    }
+
+	    sql = "select tag_id from TAG_DATA where tag_val='1234567890abcdef1234567a' AND tag_antID=2;";
+
+	    rc = sqlite3_exec(rfid_db, sql, callback, &select_tag_id, &zErrMsg);
+
+	    if( rc != SQLITE_OK ){
+	       fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	       sqlite3_free(zErrMsg);
+	    } else {
+	       fprintf(stdout, "Records created successfully\n");
+	    }
+
+	    sprintf(sql_buff, "update TAG_DATA SET tag_seen_count=5 where tag_id=%d;", select_tag_id);
+	    rc = sqlite3_exec(rfid_db, sql_buff, callback, 0, &zErrMsg);
+
+	    if( rc != SQLITE_OK ){
+	       fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	       sqlite3_free(zErrMsg);
+	    } else {
+	       fprintf(stdout, "Records created successfully\n");
+	    }
+
+	   sqlite3_exec(rfid_db, "COMMIT;", NULL, NULL, NULL);
 
 	   sqlite3_close(rfid_db);
 //	LLRP_MntServer abc;

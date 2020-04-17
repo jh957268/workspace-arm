f3a9b0e2e45df43266692c2197ba5010ee35cec0
rfidspan <44983572+rfidspan@users.noreply.github.com>
2020-04-17 16:02:16 -0700

    Update CSqlite.h

--- llrpreader/llrp-new/CSqlite.h
+++ llrpreader/llrp-new/CSqlite.h
@@ -2,26 +2,29 @@
 #define _CSQLITE_H_
 
 #include <stdio.h>
+#include <string.h>
 #include "sqlite3.h"
 
 class CSqlite
 {
     public:
 		    CSqlite(char *db);
-			~CSqlite() {sqlite3_close(rfid_db)}
+			~CSqlite() {sqlite3_close(rfid_db);}
 			
 			void begin_transaction(void);
 			void commit(void);
-			int  insert_tag(unsigned char *tag, int antid, double rssi);
+			int  insert_tag(char *tag, int antid, double rssi);
+			int  select_tag(char *tag);
+			void db_close();
 
     private:
-			int callback(void *NotUsed, int argc, char **argv, char **azColName);
-			
+
+			static int callback(void *NotUsed, int argc, char **argv, char **azColName);
 			sqlite3 *rfid_db;
-			int select_tag_id;
-			char sql_buff[128];
+			int parm[2];
+			char sql_buff[256];
 			char *sql;
-			char *zErrMsg
+			char *zErrMsg;
 };
 
 

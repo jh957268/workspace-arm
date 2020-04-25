2d4182fea5d871e439f6fc4b365d4e6c65cd64a2
rfidspan <44983572+rfidspan@users.noreply.github.com>
2020-04-25 11:49:44 -0700

    Update iMsgUtils.h

--- llrpreader/llrp-new/iMsgUtils.h
+++ llrpreader/llrp-new/iMsgUtils.h
@@ -21,6 +21,22 @@ extern "C" {
 #define IMSG_CRC_INIT			0xFF
 #define IMSG_CCITT_CRC_POLY		0x1021
 
+// Format of command or response data unit message
+/*
+	uint8_t buff [] = {HDR1, HDR2, 0x00, 0x02, 0xff, 0xfd, 0x00, 0x00};
+	command or response and real data starts at buff[6]
+	buff[6] : command or response code
+	buff[7] : real data starts from here
+	BTW (in the future): buff[7] should be status, and real data should start at buff[8]
+	data len is two bytes : count command or response and real data
+	buff[2] : high bye of data len
+	buff[3] : low byte of data len
+	buff[4] : ~high bye of data len (bit reverse)
+	buff[5] : ~low byte of data len	 (bit reverse)
+
+*/
+
+// datalen: total len in the data array + 1 (opcode)
 typedef struct iMsgObj
 {
 	u_int16_t	dataLen;

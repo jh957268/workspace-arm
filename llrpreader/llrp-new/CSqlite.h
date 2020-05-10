d32720962ddcd471ea20c024e0693d20abaef61c
rfidspan <44983572+rfidspan@users.noreply.github.com>
2020-05-09 21:06:42 -0700

    Update CSqlite.h

--- llrpreader/llrp-new/CSqlite.h
+++ llrpreader/llrp-new/CSqlite.h
@@ -16,7 +16,7 @@ class CSqlite
 			void begin_transaction(void);
 			void commit(void);
 			int  insert_tag(char *tag, int antid, double rssi);
-			int  insert_user_tag(char *tag, int antid);
+			int  insert_user_tag(char *tag, int antid, int action);
 			int  delete_user_tag(char *tag, int antid);			
 			int  select_tag(char *tag, t_func sqcallback, void *param);
 			void db_close();

3dac32bd34561af0ac7a09f3976714024b277c26
rfidspan <44983572+rfidspan@users.noreply.github.com>
2020-05-10 16:42:08 -0700

    Update post_var.cpp

--- cgi_project/cgi-bin/post_var.cpp
+++ cgi_project/cgi-bin/post_var.cpp
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include "iReaderapi.h"
+
 #define HEADER_SIZE		256
 
 int read_upd_header(char *upd_header, int len);
@@ -17,7 +19,7 @@ void send_resp(int code);
 	const char *cgi_env;
 	
 char upd_header[HEADER_SIZE + 4];
-int inserttag_main(void) 
+int inserttag_main(	IReader *handle) 
 {
     char c;
 	//FILE *fp;
@@ -27,7 +29,7 @@ int inserttag_main(void)
 	cgi_env2 = getenv("CONTENT_TYPE");
 	cgi_env3 = getenv("CONTENT_LENGTH");
 	int input_len = atoi(cgi_env3);
-	int iResult;
+	int iResult, ret;
 	
 	iResult = read_upd_header(upd_header, input_len);
 	if (iResult != input_len)
@@ -46,6 +48,14 @@ int inserttag_main(void)
 	upd_header[input_len] = 0;
 	
 	// Call the API with upd_header as passing parameter
+	ret = IReaderApiDBInsertTag(handle, upd_header);    // select all limit 0 offset 0 from table
+	if (IREADER_SUCCESS != ret)
+	{
+		//printf("command fails\n");
+		input_len = 999;
+	}
+
+	IReaderApiClose(handle);
 	send_resp(input_len);
     fflush(stdout);
 	

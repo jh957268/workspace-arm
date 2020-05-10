a9181df51aca368199e5ee9a51bb1597a01bfaed
rfidspan <44983572+rfidspan@users.noreply.github.com>
2020-05-10 16:42:18 -0700

    Update CSqlite.cpp

--- llrpreader/llrp-new/CSqlite.cpp
+++ llrpreader/llrp-new/CSqlite.cpp
@@ -77,8 +77,11 @@ CSqlite::callback(void *NotUsed, int argc, char **argv, char **azColName)
    if (NotUsed)
    {
 	   //*(int *)NotUsed = atoi(argv[0]);
-		parm[0] = atoi(argv[0]);
-		parm[1] = atoi(argv[1]);   
+	   for (i = 0; i < argc; i++)
+	   {
+		   parm[i] = atoi(argv[i]);
+		   //parm[1] = atoi(argv[1]);
+	   }
    }
    printf("\n");
    return 0;
@@ -189,7 +192,7 @@ CSqlite::insert_user_tag(char *tag, int antid, int action)
 {
 	int rc;
 	
-	sprintf(sql_buff, "select tag_id from TAG_USER_DATA where tag_val='%s' AND tag_antID=%d;", tag, antid);
+	sprintf(sql_buff, "select tag_id,tag_seen_count from TAG_USER_DATA where tag_val='%s' AND tag_antID=%d;", tag, antid);
 	parm[0] = parm[1] = 0;
 	rc = sqlite3_exec(rfid_db, sql_buff, callback, (void*)parm, &zErrMsg);
 
@@ -201,7 +204,8 @@ CSqlite::insert_user_tag(char *tag, int antid, int action)
 	}
 	if (parm[0] != 0)	
 	{
-		sprintf(sql_buff, "update TAG_USER_DATA SET tag_action=%d where tag_id=%d;", action, parm[0]);
+		int seen_cnt = parm[1] + 1;
+		sprintf(sql_buff, "update TAG_USER_DATA SET tag_action=%d,tag_seen_count=%d,tag_last_seen=datetime('now') where tag_id=%d;", action, seen_cnt, parm[0]);
 		rc = sqlite3_exec(rfid_db, sql_buff, callback, 0, &zErrMsg);
 
 	    if( rc != SQLITE_OK ){
@@ -213,9 +217,9 @@ CSqlite::insert_user_tag(char *tag, int antid, int action)
 		return rc;
 	}
 	
-	sprintf(sql_buff,"insert into TAG_USER_DATA (tag_val, tag_antID, tag_action, tag_create_time)" \
+	sprintf(sql_buff,"insert into TAG_USER_DATA (tag_val, tag_antID, tag_action, tag_first_seen, tag_last_seen, tag_seen_count)" \
 			 "VALUES" \
-	         "('%s', %d, %d, datetime('now'));",tag, antid, action); 
+	         "('%s', %d, %d, datetime('now'), datetime('now'), 1);",tag, antid, action); 
 			 
 	rc = sqlite3_exec(rfid_db, sql_buff, callback, 0, &zErrMsg);
 
@@ -225,6 +229,39 @@ CSqlite::insert_user_tag(char *tag, int antid, int action)
 	} else {
 	    fprintf(stdout, "Records inserted successfully\n");
 	}
+	return (rc);
+}
+
+int
+CSqlite::user_tag_action(char *tag, int antid, int *action)
+{
+	int rc;
+	
+	sprintf(sql_buff, "select tag_id,tag_action,tag_seen_count from TAG_USER_DATA where tag_val='%s' AND tag_antID=%d;", tag, antid);
+	parm[0] = parm[1] = parm[2] = 0;
+	rc = sqlite3_exec(rfid_db, sql_buff, callback, (void*)parm, &zErrMsg);
+
+	if( rc != SQLITE_OK ){
+	    fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	    sqlite3_free(zErrMsg);
+	} else {
+	    fprintf(stdout, "Record seleted successfully\n");
+	}
+	if (parm[0] != 0)	
+	{
+		int seen_cnt = parm[1] + 1;
+		sprintf(sql_buff, "update TAG_USER_DATA SET tag_seen_count=%d,tag_last_seen=datetime('now') where tag_id=%d;", seen_cnt, parm[0]);
+		rc = sqlite3_exec(rfid_db, sql_buff, callback, 0, &zErrMsg);
+
+	    if( rc != SQLITE_OK ){
+	       fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	       sqlite3_free(zErrMsg);
+	    } else {
+	       fprintf(stdout, "Record updated successfully\n");
+	    }
+		*action = parm[2];
+	}
+	
 	return (rc);
 }
 

a8cc3a7feacaa41b013d1cc5c9e07d87cc26f686
rfidspan <44983572+rfidspan@users.noreply.github.com>
2020-05-16 22:21:15 -0700

    Update pwm.h

--- llrpreader/llrp-new/pwm.h
+++ llrpreader/llrp-new/pwm.h
@@ -2,22 +2,35 @@
 #define _PWM_H_
 
 #include <stdio.h>
+#include "OwTask.h"
+#include "OwSemaphore.h"
 
-class PWM
+class PWM:
+	public OwTask
 {
     public:
 		        PWM();
 				~PWM();
 
+		void	main(OwTask*);
         static  PWM*	getInstance();
         void    enable(int val);
 		void 	period(int ns);
 		void	duty_cycle(int ns);
+		void  	polarity(char *pol);
+
+		void 	do_open();
+		void 	do_close();
 
     private:
         static  PWM* spInstance; ///< Points to the instance
         FILE    *fp;
-};
+
+	    static	int		semaphoreTake(int timeout);
+	    static	int		semaphoreGive();
+
+	    static OwSemaphore 		*m_hSem;
+ };
 
 
 #endif // _GPIO_H_
